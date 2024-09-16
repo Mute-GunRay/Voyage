ecf3e9d54107b5a39bea8b7e94ae36ef590a81a3
Tate Ray <tate@tateray.com>
2024-09-16 14:57:44 -0600

    add comments to map development;

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-// crew members should be composed from the following arrays
-// new crew members should become available from events
-// crew list should be viewable by clicking on an icon next to the crew size text / on the crew size text itself (perhaps could pop up a small preview modal when hovering)
+// crewmate composition arrays
 const personJobArray = [ "swordsman", "shipwright", "navigator", "cook", "doctor", "gunslinger", "musician", "archieologist"];
 const personGoalArray = ["growth", "exploration", "wealth", "revenge", "research", "fame"];
 const personExperianceArray = ["rookie", "novice", "apprentice", "skilled", "exceptional", "master"];
 const personRelationArray = ["rival", "hated", "disliked", "neutral", "liked", "loved", "friend"];
 const personReputaionArray = ["unknown", "known", "famous", "notorious", "beloved", "renowned"];
-
-// islands should be composed of various modifiers pulled randomly from the following const arrays
+// island composition arrays
 const sizeArray = ["tiny", "small", "medium", "large", "huge"];
 const populationArray = ["uninhabited", "inhabited", "sparse", "bustling", "crowded"];
 const devlopmentArray = ["undeveloped", "ramshakle", "developing", "developed", "advanced"];
 const climateArray = ["tropical", "temperate", "scorching", "chilly", "frozen"];
 const biomeArray = ["forest", "grassland", "hills", "mountains", "swamp"];
 const dangerArray = ["lethal", "dangerous", "wild", "calm", "peaceful", "joyous"];
-
-// events should be composed of various modifiers pulled randomly from the following const arrays
-// actions that can be taken in response will be generated based on the kind of event
-// events should have a chance to
-
+// event composition arrays
 const eventArray = ["fight", "monster", "settlment", "government"];
 const levelArray = ["deadly", "risky", "gamble", "fortunate", "surething"];
 const rewardArray = ["reputation", "treasure", "crew", "supplies"];
 const modifierArray = ["low", "medium", "high"];
-
-// players should be forced to stay on each island for a number of days/ticks based on distance to next island
-// each day should have a random chance to spawn an event
-// distance between islands should be a random number pulled
-// players should be able to buy supplies/items on items with a population
-
-// creatures should be composed from the following const arrays
-// relevent functions should only be called when an event needs them
+// creature composition arrays
 const baseArray = ["mammal", "fish", "reptile", "bird", "plant"];
 const creatureSizeArray = ["tiny", "small", "meduim", "larg", "huge"];
 const creatureTempermentArray = ["friendly", "aggressive", "indifferent", "afraid"];
 const creatureToughnessArray = ["flimsy", "weak", "sturdy", "tough", "beastly", "monsterous"];
 
+
+/*
+  stage 1:
+  add functions to allow for naming of captian, crew, and ship;
+  add functions to show to update player/overall stats (i.e. treasure, supplies, etc. );
+  add functions to save relevant data to local-storage;
+  add functions to show/hide modals;
+  add functions to compose islands, crew, creatures, and events;
+*/
+
+/*
+  stage 2:
+  add functions to change css selectors to create basic island visuals;
+  add functions to track time / countdown;
+  add functions to travel between islands (pick a distance to next island, then generate next island);
+  add functions for exploring islands ("rolls" for events);
+  add functions for fighting (creating attack/health/defence etc. for crew and other creatures);
+*/
+
+/*
+  stage 3: 
+  add functions to allow for progression when tab is closed;
+  add functions to allow adding/losing crew members;
+  add functions to update crewmate stats 
+ */
