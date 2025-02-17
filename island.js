51fcde8a99e1ef11d9cf64eeaca671feb1c30322
Tate Ray <tate@tateray.com>
2025-02-17 13:28:11 -0700

    removed function keyword;

--- island.js
+++ island.js
@@ -7,74 +7,74 @@ class Island {
     static danger;
     
     constructor () {
-	this.size = evaluateSize(generateRandomValue());
-	this.climate = setClimate(generateRandomValue());
-	this.population = setPopulation(generateRandomValue());
-	this.development = setDevelopment(generateRandomValue());
-	this.danger = setDanger(generateRandomValue());
+		this.size = evaluateSize(generateRandomValue());
+		this.climate = setClimate(generateRandomValue());
+		this.population = setPopulation(generateRandomValue());
+		this.development = setDevelopment(generateRandomValue());
+		this.danger = setDanger(generateRandomValue());
     }
 
-    function evaluateSize(randomValue) {
-	if(randomValue <= 25){
-	    return "small";
-	} else if(randomValue <= 50){
-	    return "meduim";
-	} else if(randomValue <= 75){
-	    return "large";
-	} else if(randomValue > 75){
-	    return "huge";
-	}
-    }
-
-        function setClimate(randomValue) {
-	if(randomValue <= 25){
-	    return "arctic";
-	} else if(randomValue <= 50){
-	    return "temprate";
-	} else if(randomValue <= 75){
-	    return "arid";
-	} else if(randomValue > 75){
-	    return "desert";
-	}
-}
-
-        function setPopulation(randomValue) {
-	if(randomValue <= 25){
-	    return "uninhabited";
-	} else if(randomValue <= 50){
-	    return "sparse";
-	} else if(randomValue <= 75){
-	    return "booming";
-	} else if(randomValue > 75){
-	    return "metropolitan";
-	}
-}
-
-        function setDevelopment(randomValue){
-	if(randomValue <= 25){
-	    return "primative";
-	} else if(randomValue <= 50){
-	    return "medieval";
-	} else if(randomValue <= 75){
-	    return "modern";
-	} else if(randomValue > 75){
-	    return "andvanced";
-	}
+	evaluateSize(randomValue) {
+		if(randomValue <= 25){
+	    	return "small";
+		} else if(randomValue <= 50){
+	    	return "meduim";
+		} else if(randomValue <= 75){
+	    	return "large";
+		} else {
+	    	return "huge";
+		}
 	}
 
-        function setDanger(randomValue){
-	if(randomValue <= 25){
-	    return "tranquil";
-	} else if(randomValue <= 50){
-	    return "risky";
-	} else if(randomValue <= 75){
-	    return "wild";
-	} else if(randomValue > 75){
-	    return "hazardous";
+    setClimate(randomValue) {
+		if(randomValue <= 25){
+	    	return "arctic";
+		} else if(randomValue <= 50){
+	    	return "temprate";
+		} else if(randomValue <= 75){
+	    	return "arid";
+		} else{
+	    	return "desert";
+		}
 	}
+
+    setPopulation(randomValue) {
+		if(randomValue <= 25){
+	    	return "uninhabited";
+		} else if(randomValue <= 50){
+	    	return "sparse";
+		} else if(randomValue <= 75){
+	    	return "booming";
+		} else{
+	    	return "metropolitan";
+		}
+	}
+
+    setDevelopment(randomValue){
+		if(randomValue <= 25){
+		    return "primative";
+		} else if(randomValue <= 50){
+	    	return "medieval";
+		} else if(randomValue <= 75){
+	    	return "modern";
+		} else{
+		    return "andvanced";
+		}
+	}
+
+	setDanger(randomValue){
+		if(randomValue <= 25){
+	    	return "tranquil";
+		} else if(randomValue <= 50){
+	    	return "risky";
+		} else if(randomValue <= 75){
+	    	return "wild";
+		} else{
+	    	return "hazardous";
+		}
     }
     
-    function generateRandomValue() {
-	return Math.floor(Math.random() * 100);
+    generateRandomValue() {
+		return Math.floor(Math.random() * 100);
     }
 }
