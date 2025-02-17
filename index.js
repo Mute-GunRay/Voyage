a4cf40d7b9fa15576fac7c7d7c8ad5e308301c68
Tate Ray <tate@tateray.com>
2025-02-17 15:12:10 -0700

    ...;

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ voyageButton.addEventListener("click", (event) => {
 
 	if(distance < 0){
 	    clearInterval(interval);
-	    island = new Island(genrateRandomValue(), generateRandomValue(), generateRandomValue, generateRanomdValue(), generateRandomValue());
+	    island = new Island(generateRandomValue(), generateRandomValue(), generateRandomValue, generateRanomdValue(), generateRandomValue());
 	    console.log( island.size);
 	    voyageButton.disabled = false;
 	    voyageButton.textContent = "Set Sail!"
