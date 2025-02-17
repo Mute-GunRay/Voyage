9c8534c4b5fddee00ed6a395f103031e4b477ff5
Tate Ray <tate@tateray.com>
2025-02-17 14:23:49 -0700

    refactored island class;

--- island.js
+++ island.js
@@ -7,74 +7,70 @@ class Island {
     static danger;
     
     constructor () {
-		this.size = evaluateSize(generateRandomValue());
-		this.climate = setClimate(generateRandomValue());
-		this.population = setPopulation(generateRandomValue());
-		this.development = setDevelopment(generateRandomValue());
-		this.danger = setDanger(generateRandomValue());
+	this.size = Math.floor(Math.random * 100);
+	this.climate = Math.floor(Math.random * 100);
+	this.population = Math.floor(Math.random * 100);
+	this.development = Math.floor(Math.random * 100);
+	this.danger = Math.floor(Math.random * 100);
     }
 
-	evaluateSize(randomValue) {
-		if(randomValue <= 25){
+	get size() {
+		if(this.size <= 25){
 	    	return "small";
-		} else if(randomValue <= 50){
+		} else if(this.size<= 50){
 	    	return "meduim";
-		} else if(randomValue <= 75){
+		} else if(this.size <= 75){
 	    	return "large";
 		} else {
 	    	return "huge";
 		}
 	}
 
-    setClimate(randomValue) {
-		if(randomValue <= 25){
+    get climate() {
+		if(this.climate <= 25){
 	    	return "arctic";
-		} else if(randomValue <= 50){
+		} else if(this.climate <= 50){
 	    	return "temprate";
-		} else if(randomValue <= 75){
+		} else if(this.climate <= 75){
 	    	return "arid";
 		} else{
 	    	return "desert";
 		}
 	}
 
-    setPopulation(randomValue) {
-		if(randomValue <= 25){
+    get population() {
+		if(this.population <= 25){
 	    	return "uninhabited";
-		} else if(randomValue <= 50){
+		} else if(this.population <= 50){
 	    	return "sparse";
-		} else if(randomValue <= 75){
+		} else if(this.population <= 75){
 	    	return "booming";
 		} else{
 	    	return "metropolitan";
 		}
 	}
 
-    setDevelopment(randomValue){
-		if(randomValue <= 25){
+    get development(){
+		if(this.development <= 25){
 		    return "primative";
-		} else if(randomValue <= 50){
+		} else if(this.development <= 50){
 	    	return "medieval";
-		} else if(randomValue <= 75){
+		} else if(this.development <= 75){
 	    	return "modern";
 		} else{
 		    return "andvanced";
 		}
 	}
 
-	setDanger(randomValue){
-		if(randomValue <= 25){
+	get danger(){
+		if(this.danger <= 25){
 	    	return "tranquil";
-		} else if(randomValue <= 50){
+		} else if(this.danger <= 50){
 	    	return "risky";
-		} else if(randomValue <= 75){
+		} else if(this.danger <= 75){
 	    	return "wild";
 		} else{
 	    	return "hazardous";
 		}
-    }
-    
-    generateRandomValue() {
-		return Math.floor(Math.random() * 100);
     }
 }
