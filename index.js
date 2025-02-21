dfcdc8cb6e113b1a647e63dbf689d821cf982bab
Tate Ray <tate@tateray.com>
2025-02-21 08:50:16 -0700

    added conversion functions to island file;

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// crewmate composition arrays
-const personJobArray = [ "swordsman", "shipwright", "navigator", "cook", "doctor", "gunslinger", "musician", "archieologist"];
-const personGoalArray = ["growth", "exploration", "wealth", "revenge", "research", "fame"];
-const personExperianceArray = ["rookie", "novice", "apprentice", "skilled", "exceptional", "master"];
-const personRelationArray = ["rival", "hated", "disliked", "neutral", "liked", "loved", "friend"];
-const personReputaionArray = ["unknown", "known", "famous", "notorious", "beloved", "renowned"];
-// event composition arrays
-const eventArray = ["fight", "monster", "settlment", "government"];
-const levelArray = ["deadly", "risky", "gamble", "fortunate", "surething"];
-const rewardArray = ["reputation", "treasure", "crew", "supplies"];
-const modifierArray = ["low", "medium", "high"];
-// creature composition arrays
-const baseArray = ["mammal", "fish", "reptile", "bird", "plant"];
-const creatureSizeArray = ["tiny", "small", "meduim", "larg", "huge"];
-const creatureTempermentArray = ["friendly", "aggressive", "indifferent", "afraid"];
-const creatureToughnessArray = ["flimsy", "weak", "sturdy", "tough", "beastly", "monsterous"];
-
 let voyageButton = document.getElementById("voyage-button");
 
 voyageButton.addEventListener("click", (event) => {
@@ -29,13 +12,8 @@ voyageButton.addEventListener("click", (event) => {
 	if(distance < 0){
 	    clearInterval(interval);
 	    island = new Island();
-	    console.log( island.size);
 	    voyageButton.disabled = false;
 	    voyageButton.textContent = "Set Sail!"
 	}
     }, 1000)
 });
-
-function generateRandomValue(){
-    return Math.floor(Math.random() * 100)
-}
--- island.js
+++ island.js
@@ -1,7 +1,71 @@
 function Island() {
-    this.size = Math.floor(Math.random() * 100);
-    this.populaiton =Math.floor(Math.random() * 100);
-    this.development = Math.floor(Math.random() * 100);
-    this.climate = Math.floor(Math.random() * 100);
-    this.danger = Math.floor(Math.random() * 100);
+    this.size = convertSizeToText(generateRandomValue());
+    this.population = convertPopulationToText(generateRandomValue());
+    this.development = convertDevelopmentToText(generateRandomValue());
+    this.climate = convertClimateToText(generateRandomValue());
+    this.danger = convertDangerToText(generateRandomValue())
+}
+
+function generateRandomValue(){
+    return Math.floor(Math.random() *  100);
+}
+
+function convertSizeToText(value){
+    if(value <= 25){
+	return "small";
+    } else if(value <= 50){
+	return "medium";
+    } else if(value <= 75){
+	return "large";
+    } else {
+	return "huge";
+    }
+}
+
+function convertPopulationToText(value){
+    if(value <= 25){
+	return "sparse";
+    } else if(value <= 50){
+	return "small";
+    } else if(value <= 75){
+	return "average";
+    } else {
+	return "metropolitan";
+    }
+}
+
+function convertDevelopmentToText(value){
+    if(value <= 25){
+	return "primative";
+    } else if(value <= 50){
+	return "medieval";
+    } else if(value <= 90){
+	return "industrial";
+    } else {
+	return "advanced";
+    }
+}
+
+function convertClimateToText(value){
+    if(value <= 25){
+	return"arctic";
+    } else if(value <= 50){
+	return "frigid";
+    } else if(value <= 75){
+	return "temperate";
+    } else {
+	return "arid";
+    } 
+}
+
+function convertDangerToText(value){
+    if(value <= 25){
+	return"tranquil";
+    } else if(value <= 50){
+	return "calm";
+    } else if(value <= 75){
+	return "dangerous";
+    } else {
+	return "savage";
+    } 
 }
